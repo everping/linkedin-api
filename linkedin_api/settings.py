09854de6aad2f600e8cb69c03de5ab0ff0818406
ping <trungstp@gmail.com>
2020-02-10 12:42:29 +0700

    Update settings.py

--- linkedin_api/settings.py
+++ linkedin_api/settings.py
@@ -1,4 +1,6 @@
 import os
+import tempfile
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.join(tempfile.gettempdir(), "linkedin")
 COOKIE_PATH = os.path.join(ROOT_DIR, "cookies/")
